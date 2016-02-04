b7e9fbb820f1c9d1c58e49318b6afa95a5499328
rpaparov <rpaparov@hotmail.com>
2016-02-03 22:34:53 -0500

    Added random shelter size from server

--- src/server/gameServer.py
+++ src/server/gameServer.py
@@ -1,6 +1,7 @@
 import socketserver
 import json
 import time
+from random import choice
 
 from common.networking import CompactJson
 
@@ -11,11 +12,26 @@ class GameServer(socketserver.ThreadingTCPServer):
 class GameServerHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		# listen
-		data = self.request.recv(64)
-		query = data.decode('utf-8')
+		data = self.request.recv(64).decode('utf-8')
+		jsonDict = json.loads(data)
 		# handle query (temporary output for debug)
 		t = time.ctime()
-		print('request at {}, query = {}'.format(t, query))
+		print('request at {}, query = {}'.format(t, data))
+		
+		if jsonDict.get('query') == 'update':
+			idd = jsonDict.get('id')
+			status = self.getStatus(idd)
 		# respond
-		data = {'status' : 'ok', 'time' : t}
-		self.request.sendall(CompactJson(data))
+		answer = {'status' : 'ok', 'time' : t}
+		answer.update(status)
+		self.request.sendall(CompactJson(answer))
+
+
+	def getStatus(self, idd):
+		'''Get last status of shelter of given id'''
+		if idd is None:
+			print('unknown shelter id')
+			return {}
+		shelterSize = (choice(range(20, 30)), choice(range(5, 10)))
+		status = {'shelterSize' : shelterSize}
+		return status
